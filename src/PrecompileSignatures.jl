a9f914e009323068005b145aa3db8d962d6c8eea
Rik Huijzer <rikhuijzer@pm.me>
2022-04-21 17:27:32 +0200

    Add `_unpack_union!`

--- src/PrecompileSignatures.jl
+++ src/PrecompileSignatures.jl
@@ -23,10 +23,48 @@ end
 _all_concrete(type::DataType) = isconcretetype(type)
 _all_concrete(types::Vector{DataType}) = isconcretetype.(types)
 
-"Return precompile directives for signature `sig`."
-function _directives(sig::DataType, split_union::Bool)
-    method, types = sig.parameters
+_pairs(@nospecialize(args...)) = vcat(Base.product(args...)...)
+
+function _unpack_union!(x::Union; out=DataType[])
+    push!(out, x.a)
+    return _unpack_union!(x.b; out)
+end
+_unpack_union!(x; out=DataType[]) = push!(out, x)
+
+"""
+    _split_union(sig::DataType)
+
+Return multiple `DataType`s containing concrete types only for each combination of concrete types that can be found.
+
+# Example
+```
+julia> f(x, y) = 3;
+
+julia> PrecompileSignatures._split_union(Tuple{f, Union{Int, Float64}, Union{Float32, String}})
+Vector
+  Tuple{f, Int, Float32}
+  Tuple{f, Int, String}
+  Tuple{f, Float64, Float32}
+  Tuple{f, Float64, String}
+```
+"""
+function _split_union(sig::DataType)
+    method, types... = sig.parameters
+    @show types
+    pairs = _pairs(types)
+end
+
+"""
+Return precompile directives datatypes for signature `sig`.
+Each returned `DataType` is ready to be passed to `precompile`.
+"""
+function _directives_datatypes(sig::DataType, split_union::Bool)
+    method, types... = sig.parameters
     _all_concrete(types) && return sig
+    out = DataType[]
+    if split_union
+        out = _split_union(sig)
+    end
     method
     types
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,9 @@ end
 
 @test P._module_functions(M) == [M.a, M.b]
 
-@test P._directives(Tuple{M.a, Int}, true) == Tuple{M.a, Int}
+@test P._unpack_union!(Union{Float64, Int64, String, Symbol}) == [Float64, Int64, String, Symbol]
+
+# @test P._directives_datatypes(Tuple{M.a, Int}, true) == Tuple{M.a, Int}
 
 directives = precompile_signatures(M)
 
